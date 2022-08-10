aa15415d3390c970f1159e46cef8339932b6a0f5
LucasHrqc <lucas.hrqc@gmail.com>
2022-08-10 14:11:08 -0300

    Using concepts learned to dev

---
+++ index.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="recursos/estilo.css">
+    <title>Título</title>
+</head>
+<body>
+    <header class="cabecalho">
+        <div><h1>Websaíti mil grauzudo</h1></div>
+        <nav class="menu">
+            <a href="">Home</a>
+            <a href="">Blog</a>
+            <a href="">Sign In</a>
+            <a href="">Stop trying, you'll fail</a>
+            <a href="">Sign Up</a>
+        </nav>
+        <div><h2>Since always, treating our clients with ignorance!</h2></div>
+    </header>
+    <header class="cabecalho2">
+        <div>
+            <h3>Botão aqui</h3>
+        </div>
+        <div class="itens">
+            <a href="">NOVIDADES</a>
+            <a href="">MASCULINO</a>
+            <a href="">INFANTIL</a>
+            <a href="">MODA ÍNTIMA</a>
+            <a href="">CALÇADOS</a>
+            <a href="">ACESSÓRIOS</a>
+        </div>
+        <nav class="menu2">
+            <form action="#" method="POST">
+                    <input type="text" name="pesquisa">
+                    <button class="botao">Pesquisar</button>
+            </form>
+        </nav>
+        <div class="menor"><h3>Done by Lukh 2022</h3></div>
+    </header>    
+    <main class="principal">
+        <div>
+            <h3>Just trying to be normal here</h3>
+        </div>
+    </main>
+    <footer class="rodape">
+        <nav>
+            <h2>Sobre nós</h2>
+            <a href="">A empresa</a>
+            <a href="">Nossa jornada</a>
+            <a href="">Portal de fornecedores</a>
+            <a href="">Relação com investidores</a>
+            <a href="">Acessoria de imprensa</a>
+            <a href="">Trabalhe conosco</a>
+            <a href=""></a>
+        </nav>
+        <nav>
+            <h2>Inovação</h2>
+            <a href="">Na prática</a>
+            <a href="">Ações e projetos</a>
+            <a href="">Notícias transformadoras</a>
+            <a href="">Produtos mais sustentáveis</a>
+            <a href="">Instituto eureka</a>
+            <a href="">Cadastro de startups</a>
+        </nav>
+        <nav>
+            <h2>Suporte</h2>
+            <a href="">Nossas lojas</a>
+            <a href="">Atendimento</a>
+            <a href="">Perguntas frequentes</a>
+            <a href="">Trocas e devoluções</a>
+            <a href="">Portal da privacidade</a>
+            <a href="">Política de privacidade</a>
+            <a href="">Termos e condições de uso</a>
+            <a href="">Regras e resultados de sorteios</a>
+            <a href="">Regulamentos</a>
+        </nav>
+        <div class="imagens">
+            <h4>Baixe nosso App!</h4>
+            <img src="png/app.png" alt="secured">
+            <h4>Formas de pagamento</h4>
+            <img src="png/pag.png" alt="vaitomanocu">
+        </div>
+    </footer>
+</body>
+</html>
